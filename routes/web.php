d4b0454eef0041750e7c60b0c08f6eafebfd432c
adiletyerkin <60466005+adiletyerkin@users.noreply.github.com>
2020-04-23 22:56:27 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,36 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
+
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/delivery', function () {
+    return view('delivery');
+});
+
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+
+Route::get('/listofusers', 'RegistrationController@allData' )->name('listofusers');
+
+
+Route::get('/edituser/{id}', 'RegistrationController@edituser' )->name('edituser');
+
+Route::post('/update/{id}', 'RegistrationController@update' )->name('update');
+
+Route::get('/delete/{id}','RegistrationController@delete' )->name('delete');
