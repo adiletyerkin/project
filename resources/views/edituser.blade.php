81838b4cfc4871f0b42e2815da82c968388b821d
adiletyerkin <60466005+adiletyerkin@users.noreply.github.com>
2020-04-24 12:44:32 +0600

    Update edituser.blade.php

--- resources/views/edituser.blade.php
+++ resources/views/edituser.blade.php
@@ -13,10 +13,10 @@
 			<label for="name">Имя: </label>
 			<input type="text" name="name" placeholder="введите имя" id="name" class="form-control" value="{{$registration -> name}}">
 		</div>
-		<div class="form-group">
+<!-- 		<div class="form-group">
 			<label for="surname">Фамилия: </label>
 			<input type="text" name="surname" placeholder="введите фамилию" id="surname" class="form-control"  value="{{$registration -> surname}}">
-		</div>
+		</div> -->
 		<div class="form-group">
 			<label for="email">Адрес e-mail: </label>
 			<input type="text" name="email" placeholder="введите email" id="email" class="form-control"  value="{{$registration -> email}}">
@@ -25,14 +25,16 @@
 			<label for="phone">Телефон: </label>
 			<input type="text" name="phone" placeholder="введите телефон" id="phone" class="form-control"  value="{{$registration -> phone}}">
 		</div> -->
-		<div class="form-group">
+<!-- 		<div class="form-group">
 			<label for="password">Пароль: </label>
 			<input type="password" name="password" placeholder="пароль" id="password" class="form-control" >
 		</div>
 		<div class="form-group">
 			<label for="password_confirmation ">Повторите пароль: </label>
-			<input type="password" name="password_confirmation" placeholder="повторите пароль" id="password_confirmation " class="form-control"  
-		</div>
+			<input type="password" name="password_confirmation" placeholder="повторите пароль" id="password_confirmation " class="form-control" >
+		</div> -->
+
+		<br>
 		<button type="submit" name="button" class="btn btn-success">Отправить</button>
 	</form>
 @endsection
