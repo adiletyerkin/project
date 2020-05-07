07ecd3b9fb78c190fdd711d38d18e4ec804511be
adiletyerkin <60466005+adiletyerkin@users.noreply.github.com>
2020-05-08 01:51:04 +0600

    watches

--- resources/views/Country/Kazakhstan.blade.php
+++ resources/views/Country/Kazakhstan.blade.php
@@ -5,6 +5,46 @@
 
 @section('content')
 
+<h2 class="collection-title home-title page-title"><span>Kazakhstan watches</span></h2>
+
+<div class="card-deck">
+  <div class="card">
+    <div class="text-center">
+    <img src="https://time4u.kz/upload/resize_cache/iblock/7eb/265_265_1/7eb002c54403b17e61db0211a07adca5.jpg" class="card-img-top"  style=" width: 140px; height: 240px; "></div>
+
+    <div class="card-body">
+      <h5 class="card-title">ORIENT FAB0000CC9</h5>
+      <p class="card-text">54 000 тг.</p>
+    </div>
+    <div class="card-footer">
+      <a href="#" class="btn btn-primary">More</a>
+       <a href="#" class="btn btn-primary" style="margin-left: 180px">BUY</a>
+    </div>
+  </div>
+  <div class="card">
+    <div class="text-center"><img src="https://time4u.kz/upload/resize_cache/iblock/d34/570_570_1/d34e95c4bf7b718fafb4edc639cead55.jpg" class="card-img-top" style=" width: 140px; height: 240px; "  ></div>
+    <div class="card-body">
+      <h5 class="card-title">ORIENT RA-KV0405S10B</h5>
+      <p class="card-text">77 500 тг.</p>
+    </div>
+    <div class="card-footer">
+      <a href="#" class="btn btn-primary">More</a>
+      <a href="#" class="btn btn-primary" style="margin-left: 180px">BUY</a>
+    </div>
+  </div>
+  <div class="card">
+    <div class="text-center"><img src="https://time4u.kz/upload/resize_cache/iblock/216/570_570_1/216e9b80c81ad514bcb0e67af9c296f6.jpg" class="card-img-top"  style=" width: 140px; height: 240px; "></div>
+    <div class="card-body">
+      <h5 class="card-title">ADRIATICA A8243.1221QF</h5>
+      <p class="card-text">77 800 тг.</p>
+    </div>
+    <div class="card-footer">
+        <a href="#" class="btn btn-primary">More</a>
+        <a href="#" class="btn btn-primary" style="margin-left: 180px">BUY</a>
+    </div>
+  </div>
+</div>
+
 
 
 
