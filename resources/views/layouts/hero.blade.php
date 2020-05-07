b3f4a20b8b5d2a6bbb23725a98e33df987636db7
adiletyerkin <60466005+adiletyerkin@users.noreply.github.com>
2020-05-08 03:02:45 +0600

    Update Germany.blade.php

--- resources/views/layouts/hero.blade.php
+++ resources/views/layouts/hero.blade.php
@@ -1,6 +1,6 @@
 <div class="jumbotron">
 	<div class="container">
 		<h1>МЫ ОФИЦИАЛЬНЫЕ ПРЕДСТАВИТЕЛИ БРЕНДОВ НА ТЕРРИТОРИИ КАЗАХСТАНА</h1>
-		<p>Something else else</p>
+		<!-- <p>Something else else</p> -->
 	</div>
 </div>
