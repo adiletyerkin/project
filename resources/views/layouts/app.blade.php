08033f2a82448bb682adcd3965c82438780072ae
adiletyerkin <60466005+adiletyerkin@users.noreply.github.com>
2020-05-08 21:24:31 +0600

    Revert "Merge branch 'master' of https://github.com/adiletyerkin/project"
    This reverts commit 923916ba3a43911eaaf313bac2306bfd486b50cb, reversing
    changes made to e7f16d7270be9b08d9e92080259cd6c1876629fe.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,11 +62,11 @@ new Darkmode().showWidget();
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Войти') }}</a>
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Регистрация') }}</a>
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li> 
                             @endif
                         @else
@@ -86,11 +86,11 @@ new Darkmode().showWidget();
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('password.change') }}">Изменить пароль</a>
+                                    <a class="dropdown-item" href="{{ route('password.change') }}">Change Password</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Выйти') }}
+                                        {{ __('Logout') }}
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
