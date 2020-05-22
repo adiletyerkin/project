dd1086e8cf634f48290f6cb83b829ef52549a492
adiletyerkin <60466005+adiletyerkin@users.noreply.github.com>
2020-05-22 08:24:37 +0600

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -110,6 +110,7 @@ class ProductController extends Controller
      * @param  \App\Product  $product
      * @return \Illuminate\Http\Response
      */
+    public function destroy($id)
 
     {
         Product::find($id)->delete();
